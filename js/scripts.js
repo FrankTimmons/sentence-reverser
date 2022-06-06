544bd58ae13fdfb4969d2418eef41dc6e52f010b
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-06 15:28:06 -0700

    Add everything.
    Co-authored-by: alex-mcknight11 <alex.mcknight11@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 $(document).ready(function () {
   $("form#sentence").submit(function(event){
     event.preventDefault();
-    const sentence =$("#sentenceInput").val();
+    const sentenceString =$("#sentenceInput").val();
 
-    
+    const space = ' '
+
+    sentenceArray = sentenceString.split(space);
+    let newSentenceArray = [];
+    console.log(newSentenceArray);
+
+    sentenceArray.forEach(function (word){
+
+      if (word.length >= 3)
+      {
+        newSentenceArray.push(word);
+      }
+      
+    });
+
+    reversedArray = newSentenceArray.reverse();
+
+    finalString = reversedArray.join(space);
+
+    $("#results").text(finalString);
+
   });
 });
